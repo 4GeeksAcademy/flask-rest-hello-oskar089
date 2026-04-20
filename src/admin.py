ed385edb3b1d406c75053ddf5455aee999704060
Oscar Becerra <46764350+oskar089@users.noreply.github.com>
2026-04-20 18:10:44 +0000

    Corregido

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User, Post, Comment, Media, Follower
+from models import db, User, Post, Comment, Follower
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -13,7 +13,6 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Post, db.session))
     admin.add_view(ModelView(Comment, db.session))
-    admin.add_view(ModelView(Media, db.session))
     admin.add_view(ModelView(Follower, db.session))
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
